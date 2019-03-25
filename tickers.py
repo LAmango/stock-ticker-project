eaecf9aad82e0933aa03ef0360b239ee514c01c2
Alan Harlow <harlowashx00@gmail.com>
2019-03-24 22:35:15 -0400

    Added testing for is_valid and fetching price from server. Found bug and resolved for this.

--- tickers.py
+++ tickers.py
@@ -22,6 +22,7 @@
 		value of n will be <= 150
 '''
 
+from iex import Stock
 import sys
 import json
 import requests
@@ -31,43 +32,66 @@ import pandas
 # reurns false to skip that line.
 def is_valid(symbol):
     try:
+        Stock(symbol).price()
         return True
     except:
         return False
 
-def save_tickers(n):
+def get_tickers():
+    # This is used for testing and debug. This allows us to mock response from server.
+    # content = get_tickers_from_file()
+
+    content = get_tickers_live()
+    return content
+
+def get_tickers_live():
     # Sets URL for stock information to pull from
     url = 'https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&render=download'
     response = requests.get(url)
 
     if response.status_code == 200:
         # pulls all data from CSV file into string
-        content = response.content.decode('utf-8')
-
-        # splits string into a list at the newline feed from CSV info
-        lines = content.split("\n")
-
-
-        first_line = True
-        lines_output = 0
-
-        # create ticker file if needed
-        fp = open("ticker.txt", "w")
-
-        for line in lines:
-            if lines_output == n: #
-                break
-            if not first_line: # check to see if we are currently on the first line
-                symbol = line.split(',', 1)[0].replace('"', '')
-                if is_valid(symbol):
-                    fp.write(line) # write to file
-                    lines_output += 1
-            else:
-                # if we are on the first line, skip header
-                first_line = False
-
+        return response.content.decode('utf-8')
     else:
-        print(response.status_code)
+        raise RuntimeError("requests.get("+url+") returned status code " + str(response.status_code))
+
+# used for debug of CSV file.
+def get_tickers_to_file():
+    fp = open("tickers.csv","w")
+    fp.write(get_tickers_live())
+    fp.close()
+
+# used for debug of CSV file.
+def get_tickers_from_file():
+    fp = open("tickers.csv","r")
+    content = fp.read()
+    fp.close()
+    return content
+
+
+def save_tickers(n):
+    content = get_tickers()
+    # splits string into a list at the newline feed from CSV info
+    lines = content.split("\n")
+
+
+    first_line = True
+    lines_output = 0
+
+    # create ticker file if needed
+    fp = open("ticker.txt", "w")
+
+    for line in lines:
+        if lines_output == n: #
+            break
+        if not first_line: # check to see if we are currently on the first line
+            symbol = line.split(',', 1)[0].replace('"', '')
+            if is_valid(symbol):
+                fp.write(line+'\n') # write to file
+                lines_output += 1
+        else:
+            # if we are on the first line, skip header
+            first_line = False
 
 
 if __name__ == "__main__":
